09e57428e087140ffa4f287d5b231691928151eb
Максим Фетисов <makcim220620072016@gmail.com>
2026-04-14 13:03:01 +0700

    del

--- test for staples.py
+++
@@ -1,6 +0,0 @@
-import staples
-assert staples.is_balance(['{''('')''[''('')'']''}''<''<''<''>''>''>']), "Не робит"
-assert staples.is_balance(['a''(''b''[''c''{''d''}''e'']''f'')''g']), "Не робит"
-assert staples.is_balance([]), "Не робит"
-assert staples.is_balance(['(''(''(']), "Error"
-assert staples.is_balance(['[''('']'')']), "Error"
